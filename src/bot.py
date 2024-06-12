71c4775a9eaa716da3a78cf70081f0213ac5c684
Faye Billye <fbillye@student.21-school.ru>
2024-06-13 00:11:59 +0700

    Update bot

--- src/bot.py
+++ src/bot.py
@@ -12,7 +12,11 @@ from aggregation import Aggregation
 load_dotenv()
 
 
-async def get_result(message: types.Message, aggregation: Aggregation):
+async def get_result(message: types.Message, aggregation: Aggregation) -> None:
+    """
+    Получение сообщения от пользователя, вызов метода для получения результата
+    агрегации данных и отправка результата вычисления пользователю.
+    """
     try:
         result = await aggregation.get_result(json.loads(message.text))
         await message.answer(str(result), parse_mode=ParseMode.MARKDOWN)
@@ -20,7 +24,11 @@ async def get_result(message: types.Message, aggregation: Aggregation):
         await message.answer('Невалидный запрос. Пример запроса:\n{"dt_from": "2022-09-01T00:00:00", "dt_upto": "2022-12-31T23:59:00", "group_type": "month"}')  # noqa: E501
 
 
-async def start_bot(token: str, aggregation: Aggregation):
+async def start_bot(token: str, aggregation: Aggregation) -> None:
+    """
+    Запуск телеграм бота, устанавка обработчика handle_all_messages для всех
+    входящих сообщений.
+    """
     bot = Bot(token=token)
     dp = Dispatcher(bot)
     dp.middleware.setup(LoggingMiddleware())
